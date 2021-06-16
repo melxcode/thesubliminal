137c8c104a34a28871cfdc58320f82a523859059
Uriel De Carlini <uriel@acuidado>
2021-06-16 18:36:10 -0300

    add file

---
+++ src/constants/URLS.js
@@ -0,0 +1,35 @@
+export const brandingUrls = [
+  "https://fotos.subefotos.com/644906e25d9fae63d55191a906996346o.png",
+  "https://fotos.subefotos.com/c66a9d97de0a1c7d89240ee41de0df9bo.png",
+  "https://fotos.subefotos.com/252b60d97d3f0c4117501a90ad12d867o.png",
+  "https://fotos.subefotos.com/c41971d1728dda991653e00180eac077o.png",
+  "https://fotos.subefotos.com/aaac4e754f7b5b5ee47bb004004b86abo.png",
+  "https://fotos.subefotos.com/e179aa5a1358c3bb2f68af4514de1732o.png",
+  "https://fotos.subefotos.com/cfa73ad59b8010559949ec30b55f92f3o.png",
+  "https://fotos.subefotos.com/75aa60047b04fa2633ce6693f4aef943o.png",
+  "https://fotos.subefotos.com/1634399cd2b40856ec15977e6278bc4fo.png",
+  "https://fotos.subefotos.com/0932dbaf2658c672d18cb70a976b9b26o.png",
+];
+
+export const marketingUrls = [
+  "https://fotos.subefotos.com/3f391f4b0c38e8708baa49a7d95dfc4co.png",
+  "https://fotos.subefotos.com/0dcc9d96e1b3af7b5e25a36483a20378o.png",
+  "https://fotos.subefotos.com/aa16377f51f132073e324fce5678033co.png",
+  "https://fotos.subefotos.com/fb5989f87fc4d2fef99e16b394cc7833o.png",
+];
+
+export const devUrls = [
+  "https://fotos.subefotos.com/c96934be5db18f5a72c1e06040a67818o.jpg",
+  "https://fotos.subefotos.com/dfe5f9aa452e872aa6250726d69ddf04o.jpg",
+  "https://fotos.subefotos.com/00fc525edf640475b2862dfacdb42869o.jpg",
+  "https://fotos.subefotos.com/091f72ef63a6b0c11df5b424140eadc2o.png",
+  "https://fotos.subefotos.com/5174413a42f0769a03cf03eb7cdc9367o.png",
+];
+
+export const uxUrls = [
+  "https://fotos.subefotos.com/1f75e613dc05d35fead67f340d058e86o.jpg",
+  "https://fotos.subefotos.com/5174413a42f0769a03cf03eb7cdc9367o.png",
+];
+
+export const logo =
+  "https://fotos.subefotos.com/4a1a035ae7f9d4cdf22f480e33f73913o.png";
