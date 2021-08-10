bee3d9df2157831fde6179dae5328006d1d79278
Uriel De Carlini <uriel@acuidado>
2021-08-09 23:05:59 -0300

    fix calendar

--- src/components/calendly/index.js
+++ src/components/calendly/index.js
@@ -5,19 +5,41 @@ const CalendlyWidget = ({ username, duration }) => {
   return (
     <div>
       <InlineWidget
-        url={`https://calendly.com/${username}/${duration}`}
+        url={`https://calendly.com/bysubliminal/15min`}
         styles={{
           height: "1000px",
         }}
-        pageSettings={{}}
+        pageSettings={{
+          backgroundColor: "ffffff",
+          hideEventTypeDetails: false,
+          hideLandingPageDetails: false,
+          primaryColor: "00a2ff",
+          textColor: "4d5055",
+        }}
         prefill={{
-          email: "example@mail.com",
-          firstName: "",
-          lastName: "",
-          name: "",
+          email: "test@test.com",
+          firstName: "Jon",
+          lastName: "Snow",
+          name: "Jon Snow",
           customAnswers: {
-            a1: "Lets talk about your idea ! ",
+            a1: "a1",
+            a2: "a2",
+            a3: "a3",
+            a4: "a4",
+            a5: "a5",
+            a6: "a6",
+            a7: "a7",
+            a8: "a8",
+            a9: "a9",
+            a10: "a10",
           },
+        }}
+        utm={{
+          utmCampaign: "Spring Sale 2019",
+          utmContent: "Shoe and Shirts",
+          utmMedium: "Ad",
+          utmSource: "Facebook",
+          utmTerm: "Spring",
         }}
       />
     </div>
